08183090a1d0b14e88bcc093eb822683a9b89fce
chrisrober011 <chris.rober011@gmail.com>
2024-06-17 22:36:46 +0300

    HalfCoding

---
+++ pkg/config/app.go
@@ -0,0 +1,24 @@
+/* this will help to connect with mysql database */
+
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:root1234@/BookStore?charset=utf8&SpareTime=true&loc=Local") //first calling mysql the username:Password@/and the rest code
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *grom.DB{
+	return db
+}
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,18 @@
+/* this help to visualize the routes where the users will hit 
+from their front end and postman and the user will hits this routes */
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/chrisrober011/BookStore-GO/pkg/controllers"
+
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methos("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methos("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	
+}
